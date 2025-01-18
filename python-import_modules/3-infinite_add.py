6fc9dbe4a861a83a7ef390c199e5c7ea519273a3
DMC254 <mercadodiego2006@gmail.com>
2025-01-18 18:11:40 -0400

    Task 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,7 +2,12 @@
 
 import sys
 
-total = 0
-for i in range(1, len(sys.argv)):
-    total += int(sys.argv[i])
-print("{}".format(total))
+if __name__ == "__main__":
+    av = sys.argv
+    l_av = len(av)
+    sum = 0
+
+    if l_av > 1:
+        for i in range(1, l_av):
+            sum += int(av[i])
+    print(sum)
