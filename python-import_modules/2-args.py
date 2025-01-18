0195c6f06abe9b5f47a5dfce77c7fe8484279f66
DMC254 <mercadodiego2006@gmail.com>
2025-01-18 17:40:43 -0400

    ix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
+import sys
 
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
+if __name__ == "__main__":
+    argv = sys.argv[1:]  # Excluding the script name from the arguments
+    arg_count = len(argv)
+
+    if arg_count == 0:
+        print("Number of argument(s): 0.")
     else:
-        print("{} arguments:".format(count))
-    for i in range(1, count + 1):
-        print("{}: {}".format(i, sys.argv[i]))
+        print(f"Number of argument(s): {arg_count} argument{'s' if arg_count > 1 else ''}:")
+        for i, arg in enumerate(argv, start=1):
+            print(f"{i}: {arg}")
