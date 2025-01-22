b2e889624cbb97db1e579dd2452276ab67cf70d7
DMC254 <mercadodiego2006@gmail.com>
2025-01-22 19:34:19 -0400

    fix

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     new_str = ''
     for i in my_string:
-        if i != 'c' and i != 'c':
+        if i != 'C' and i != 'c':
             new_str += i
-    
+
     return new_str
