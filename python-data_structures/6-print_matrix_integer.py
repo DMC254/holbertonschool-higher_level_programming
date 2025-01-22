db8c9a9a0dfdb2f76029d95a41e5fde4404339d4
DMC254 <mercadodiego2006@gmail.com>
2025-01-22 19:21:01 -0400

    fix 2

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_interger(matrix=[[]]):
-    if matrix:
-        for elems in matrix:
-            i = 1
-            length = len(elems)
+    for row in matrix:
+        print(" ".join("{:d}".format(num) for num in row))
 
-            for elem in elems:
-                if i == length:
-                    print('{:d}'.format(elem), end='')
-                else:
-                    print('{:d}'.format(elem), end=' ')
-                i += 1
-
-            print()
+if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+    print_matrix_interger(matrix)
