9adb992988938c84199d7bb508353d5077c8c0a2
DMC254 <mercadodiego2006@gmail.com>
2025-01-23 14:13:41 -0400

    Task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
+    if type(roman_string) is not str or roman_string is None:
         return 0
-    
+
     roman_letters = [
         ['M', 1000], ['D', 500], ['C', 100], ['L', 50],
         ['X', 10], ['V', 5], ['I', 1]
@@ -15,7 +15,7 @@ def roman_to_int(roman_string):
             if letter == elem[0]:
                 if last == 0 or last >= elem[1]:
                     num += elem[1]
-                else last < elem [1]:
+                elif last < elem[1]:
                     num += elem[1] - (last * 2)
 
                 last = elem[1]
