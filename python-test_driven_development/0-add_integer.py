73dd50ada55a853cc849a1f95b6e5626fa8676d4
DMC254 <mercadodiego2006@gmail.com>
2025-01-26 20:39:51 -0400

    fix 2

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+This module provides a function `add_integer` that adds two integers.
+
+The function validates its input to ensure it only accepts integers or floats,
+and it casts floats to integers before performing the addition.
+"""
+
 def add_integer(a, b=98):
     """
     Adds two integers.
