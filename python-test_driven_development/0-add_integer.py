7edb3160f01b8a57218166035403f0fd05488bd2
DMC254 <mercadodiego2006@gmail.com>
2025-01-26 20:29:27 -0400

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,49 +1,11 @@
 #!/usr/bin/python3
-"""A module that adds two integers
-
-This module performs the addition operation betwee two numbers,
-these numbers can be intergers or floats.
-
-"""
-
-
 def add_integer(a, b=98):
-    """Adds two numbers
-    
-    Performs the addition between two numbers.
-    
-    Args:
-        a (:obj:`int, float`): The first two numbers.
-        b (:obj:`int, float`, optional): The second number.
-        
-    Returns:
-        int: The result of the addition.
-        
-    """
-    if type(a) not in (int, float):
-        raise TypeError('a must be an interger')
-    
-    if type(b) not in (int, float):
-        raise TypeError('b must be an interger')
-    
-    a = convert_to_int(a)
-    b = convert_to_int(b)
-    return a + b
-
-def convert_to_int(num):
-    """Cast the data type of num parameter
-
-    Convert a float number to a integer number
-
-    Args:
-        num (:obj:`int, float`): The number to cast.
-
-    Returns:
-        int: The number casted to integer.
-
-    """
-    if type(num) is float:
-        num = int(num)
-        return num
-    
-    return num
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return (a + b)
