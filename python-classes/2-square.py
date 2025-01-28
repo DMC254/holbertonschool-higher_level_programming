c45620815acda4c273029ffeb1165db0b8bf1e23
DMC254 <mercadodiego2006@gmail.com>
2025-01-28 19:27:23 -0400

    fix

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -2,12 +2,13 @@
 """Square generation module for Python project 0x06
 """
 
+
 class Square:
-    """Class defined for generation
+    """class defined for square generation
     """
     def __init__(self, size=0):
         if type(size) is not int:
-            raise TypeError('size must be an interger')
+            raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
