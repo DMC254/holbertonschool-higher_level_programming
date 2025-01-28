c7f42257911b81cc3ebe8423325a7a372a688adb
DMC254 <mercadodiego2006@gmail.com>
2025-01-28 18:40:21 -0400

    Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Define a square class."""
+
+class Square:
+    """
+    A class that represents a square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new square.
+
+        Args:
+            size: the size of the square.
+            """
+        self.__size = size
