e8632f9718ff206cf3c974d3486ddb6a479a40e8
DMC254 <mercadodiego2006@gmail.com>
2025-01-28 19:45:19 -0400

    Task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Square generation module for Python project
+"""Square generation module for Python project 0x06
 """
 
 
 class Square:
-    """Class defined for square generation
+    """Class defined for square generation.
 
     Args:
         size (int): length of one side of square
@@ -13,26 +13,26 @@ class Square:
         __size (int): length of one side of square
 
     Raises:
-    TypeError: if size not an interger
-    ValueError: if size is less than 0
+        TypeError: if size is not an integer
+        ValueError: if size is less than 0
 
     """
     def __init__(self, size=0):
         if type(size) is not int:
-            raise TypeError('size must be an interger')
+            raise TypeError('size must be an integer')
         if size < 0:
-            raise ValueError('size must be >=0')
+            raise ValueError('size must be >= 0')
         self.__size = size
 
     def area(self):
         """Calulates area of square.
-        
+
         Attributes:
             __size (int): length of one side of square
-            
+
         Returns:
             area (int): length of one side, squared
-        
+
         """
         area = self.__size * self.__size
         return area
