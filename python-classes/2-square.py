8efac5e711f774c1802b27dcee9b9d0385c89d49
DMC254 <mercadodiego2006@gmail.com>
2025-01-28 18:46:09 -0400

    add

---
+++ python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square generation module for Python project 0x06
+"""
+
+class Square:
+    """Class defined for generation
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError('size must be an interger')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
