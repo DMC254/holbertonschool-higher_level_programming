473c294b21c6737da52006e744e1b5cd5e254935
DMC254 <mercadodiego2006@gmail.com>
2025-02-03 19:50:49 -0400

    Task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    @classmethod
+    def area(self):
+        """Method for calculated area"""
+
+        raise Exception("area() is not implemented")
