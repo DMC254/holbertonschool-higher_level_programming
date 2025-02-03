36259ecf4b324e38d9dbea19075dc4f5f216bbc4
DMC254 <mercadodiego2006@gmail.com>
2025-02-03 19:42:27 -0400

    Task 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def is_kind_of_class(obj, a_class):
+    """Method that returns True if the object is an instance or inherited instance of a class"""
+
+    return isinstance(obj, a_class)
