76d28fb78621dd5a5981cc7d7655578bc6ea8d70
DMC254 <mercadodiego2006@gmail.com>
2025-02-04 09:39:04 -0400

    Task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+    def __init__(self, width, height):
+        self.integer_validator("width, height", width, height)")
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
