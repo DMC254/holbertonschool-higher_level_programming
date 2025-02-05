de9cb48e1ef7439c8f67dc105f72662b23fa7f71
DMC254 <mercadodiego2006@gmail.com>
2025-02-05 10:37:35 -0500

    fix

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -6,7 +6,7 @@ module with class BaseGeometry
 """
 
 
-Rectangle = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
