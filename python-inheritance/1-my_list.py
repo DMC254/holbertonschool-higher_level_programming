c39073ba8293568239e526d7d913c106e10c3e5a
DMC254 <mercadodiego2006@gmail.com>
2025-02-05 13:48:36 -0500

    fix

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Defines a class named MyList that inherits from list."""
+"""MyList"""
 
 
 class MyList(list):
     """MyList"""
-    pass
 
     def print_sorted(self):
         """Prints sorted list."""
