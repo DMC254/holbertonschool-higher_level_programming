ffa73035261d088791c6945ae550ab0f838354bb
DMC254 <mercadodiego2006@gmail.com>
2025-02-05 09:58:22 -0500

    fix

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -10,7 +10,7 @@ class MyList(list):
     """Class with method print_sorted"""
     pass
 
-def print_sorted(self):
-    """Method that sorted a list"""
-    
-    print(sorted(list(self)))
+    def print_sorted(self):
+        """Methot that sorted a list"""
+
+        print(sorted(list(self)))
