7a460b47df6f71d7c3a5497eb3ac186e4fdbca2c
DMC254 <mercadodiego2006@gmail.com>
2025-02-05 11:46:48 -0500

    fix

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -15,9 +15,9 @@ class Square(Rectangle):
     def __init__(self, size):
         """Method for initialized the attrubutes"""
 
-        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         """rectangle area"""
