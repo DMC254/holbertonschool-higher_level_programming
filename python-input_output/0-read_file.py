13c834659f36c462f62702475bcdbb1b69a3358e
DMC254 <mercadodiego2006@gmail.com>
2025-02-11 10:30:18 -0500

    Task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-
+"""Read a file"""
 
 def read_file(filename=""):
+    """Reads a text file and prints it to stdout.
+
+    Args:
+        filename (str): Name of the file to read. If not provided,
+        defaults to an empty string.
+    """
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
