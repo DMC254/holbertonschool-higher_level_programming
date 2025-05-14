6955065e2a5c47bf7ff8b107fad1c1ec1ab302f2
Diego <mercadodiego2006@gmail.com>
2025-05-14 14:20:43 -0400

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,7 +5,7 @@ It adds two integers or floats, casting them to integers if necessary.
 """
 
 
-def add_interger(a, b=98):
+def add_integer(a, b=98):
     """
     Adds two integers or floats, casting them to integers if necessary.
 
@@ -21,10 +21,10 @@ def add_interger(a, b=98):
         TypeError: If `a` or `b` is not an integer or float.
     """
     if not isinstance(a, (int, float)):
-        raise TypeError("a must be a interger")
+        raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
-        raise TypeError("b must be a interger")
-    
+        raise TypeError("b must be an integer")
+
     a = int(a)
     b = int(b)
     return a + b
