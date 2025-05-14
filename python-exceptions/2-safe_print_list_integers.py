f64d5a7df6dec24dd16edf625fe07cbc32eecbe6
Diego <mercadodiego2006@gmail.com>
2025-05-14 13:49:46 -0400

    fix 1

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def safe_print_list_intergers(my_list=[], x=0):
+def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
@@ -8,5 +8,5 @@ def safe_print_list_intergers(my_list=[], x=0):
             count += 1
         except (TypeError, ValueError):
             continue
-        print()
-        return count
+    print()
+    return count
