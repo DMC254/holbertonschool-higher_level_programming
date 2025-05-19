c49345b716fb86aa35e31a18e3cc16a5bc923708
Diego <mercadodiego2006@gmail.com>
2025-05-19 15:10:07 -0400

    task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Defines a square class."""
+
+
+class Square:
+    """
+    A class that defines a square.
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a new sqaure.
+
+        Args:
+            size: the size of the square.
+
+            Raises:
+            TypeError: if size is not an integer.
+            ValueError: if size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an interger")
+        if size < 0:
+            raise ValueError("must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            The area of the square.
+        """
+        return self.size ** 2
+    
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an interger")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
