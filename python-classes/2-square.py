d1e59cedbfd7d37be1b36b7da3f2c00b4a99f22c
Diego <mercadodiego2006@gmail.com>
2025-05-19 14:52:49 -0400

    fix

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-"""Define a square class"""
+"""Defines a square class."""
 
 
 class Square:
     """
-    A class that defines a square
+    A class that defines a square.
     """
     def __init__(self, size=0):
         """
-        Initialize a new square.
-        
+        Initializes a new sqaure.
+
         Args:
             size: the size of the square.
-            
+
             Raises:
-            TypeError: if size is not an interger.
+            TypeError: if size is not an integer.
             ValueError: if size is less than 0
         """
         if not isinstance(size, int):
-            raise TypeError("size must be an interger")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
