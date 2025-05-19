e6ede54da31ce39d3b0dc9850415eb1e2d1e3d13
Diego <mercadodiego2006@gmail.com>
2025-05-19 13:09:26 -0400

    task 2

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Define a square class."""
+
+
+class Square:
+    """
+    A class taht defines a square.
+    """
+    def __init__(self, size):
+        """
+        Initializes a new squeare.
+        
+        Args:
+            size: the size of square.
+        """
+        self.__size = size
