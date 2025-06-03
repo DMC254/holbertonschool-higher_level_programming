98cd70e40c951b45b361a7ee430487be51777f5c
Diego <mercadodiego2006@gmail.com>
2025-06-03 10:12:25 -0400

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,5 +9,5 @@ def read_file(filename=""):
         filename (str): name of the file to read
     """
 
-    with open(filename, "r", endcoding="utf-8") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         print(file.read(), end="")
