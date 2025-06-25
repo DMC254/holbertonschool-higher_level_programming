fac2ef07af08913ea34964a596388354a9d1153e
Diego <mercadodiego2006@gmail.com>
2025-06-25 16:30:31 -0400

    task 16 fix

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Script lists all shows and all genre linked to that show from the database hbtn_0d_tvshows
-SELECT tv_shows.titles, tv_genres.name
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
