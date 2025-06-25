1b3ff71cbc382f993a7a9f0d2f0af01f8e851e57
Diego <mercadodiego2006@gmail.com>
2025-06-25 15:44:32 -0400

    task 9

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,2 @@
+-- Script lists all cities contained in database hbtn_0d_usa
+SELECT cities.id, cities.name, state.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
