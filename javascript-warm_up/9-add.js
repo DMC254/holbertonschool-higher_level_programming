3533ad8096539b203008ee4a65a802601fd559d6
Diego <mercadodiego2006@gmail.com>
2025-07-09 17:16:46 -0400

    task 9

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -5,6 +5,6 @@ function add (a, b) {
 }
 
 const first = parseInt(process.argv[2], 10);
-const second = parseInt(process.argv[3], 10);\
+const second = parseInt(process.argv[3], 10);
 
 console.log(add(first, second));
